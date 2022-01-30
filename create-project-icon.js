6a8533df3c0b79ab1681ebd73e87cb42d2f42fc5
Priyal <priyal.chauhan34@gmail.com>
2022-01-30 18:47:08 +0530

    some changes

--- create-project-icon.js
+++ create-project-icon.js
@@ -21,6 +21,28 @@ function(){
     document.querySelector(".bg-modal2").style.display= "none";
 });
 
+//-----local storage------
+const arr = new Array();
+function addData(){
+    getData();
+    arr.push({
+        input1:document.getElementById("input1").value,
+        emails:document.querySelectorAll("[type=email]")
+
+        
+    });
+
+    localStorage.setItem("localData", JSON.stringify(arr));
+
+}
+console.log(arr)
+
+function getData(){
+    const str = localStorage.getItem("localData");
+    if(str != null)
+        arr= JSON.parse(str);
+}
+
 //dynamically generated email input field
 
     function  emailGen(){
@@ -29,32 +51,43 @@ function(){
         while (container.hasChildNodes()) {
             container.removeChild(container.lastChild);
         }
-        for(a=0; a<quant; a++){
+        
+        if(quant > 10){
+            alert("please keep it low");
+        } else{
+            for(a=0; a<quant; a++){
             var emailGen = document.createElement("input");
             emailGen.type="email";
             emailGen.id="email"+ a;
+            emailGen.name = "email" +a;
             emailGen.placeholder="Team Member's Email";
             container.appendChild(emailGen);
+            
         }
-    
+        }
+   
     };
+
+//prevent form clicking button if value is more than 20
+
 
 //submit event
 form.addEventListener("submit", tableGen);
 
 //function for table generator
 function tableGen(e){e.preventDefault();
-    var body = document.getElementsByTagName("body")[0];
+    var body = document.querySelector("body");
+    const tblContainer = document.getElementById("tbl-Container");
+    const tbldiv = document.createElement("div");
+    tbldiv.id = "tbldiv";
     var table = document.createElement("table");
     table.setAttribute("id", "tableid");
     var tblbody = document.createElement("tbody");
 
     var input1 = document.querySelector("#input1").value;
     var quan   = document.querySelector("#quantity").value;
-    var input2 = document.querySelector("#email0").value;
-    var input3 = document.querySelector("#email1").value;
-    var input4 = document.querySelector("#email2").value;
-    
+
+    const emails = container.querySelectorAll("[type=email]");
 
     //table rows loop
     for(i=0; i<=quan; i++){
@@ -69,28 +102,53 @@ function tableGen(e){e.preventDefault();
                 tbldata.appendChild(pname);
                 }
             if(i==1 && j==0){
-                    var tname = document.createTextNode(input2);
+                    var tname = document.createTextNode(emails[0].value);
                     tbldata.appendChild(tname);
                     }if(i==2 && j==0){
-                        var tname = document.createTextNode(input3);
+                        var tname = document.createTextNode(emails[1].value);
                         tbldata.appendChild(tname);
                          }if(i==3 && j==0){
-                            var tname = document.createTextNode(input4);
+                            var tname = document.createTextNode(emails[2].value);
                             tbldata.appendChild(tname);
-                        }
+                        }if(i==4 && j==0){
+                            var tname = document.createTextNode(emails[3].value);
+                            tbldata.appendChild(tname);
+                            }if(i==5 && j==0){
+                                var tname = document.createTextNode(emails[4].value);
+                                tbldata.appendChild(tname);
+                                }if(i==6 && j==0){
+                                    var tname = document.createTextNode(emails[5].value);
+                                    tbldata.appendChild(tname);
+                                    }
+                                    if(i==7 && j==0){
+                                        var tname = document.createTextNode(emails[6].value);
+                                        tbldata.appendChild(tname);
+                                        }if(i==8 && j==0){
+                                            var tname = document.createTextNode(emails[7].value);
+                                            tbldata.appendChild(tname);
+                                            }if(i==9 && j==0){
+                                                var tname = document.createTextNode(emails[8].value);
+                                                tbldata.appendChild(tname);
+                                                }if(i==10 && j==0){
+                                                    var tname = document.createTextNode(emails[9].value);
+                                                    tbldata.appendChild(tname);
+                                                    }
                     tblrow.appendChild(tbldata);
             }
 
         tblbody.appendChild(tblrow);
 
         }
+        console.log
 table.appendChild(tblbody);
-body.appendChild(table);
-
-document.querySelector(".bg-modal2").style.display = "none";
-
-};
-
+tbldiv.appendChild(table);
+tblContainer.appendChild(tbldiv);
+body.appendChild(tblContainer);
+    
+document.querySelector(".bg-modal2").style.display="none";
+document.getElementById("container").innerHTML = "";
+form.reset();
+}
 
 
 
--- rough-code.js
+++ rough-code.js
@@ -1,40 +1,78 @@
 if(i==2 && j==0){
     var tname = document.createTextNode(input3);
     tbldata.appendChild(tname);
-    
-}if(i==3 && j==0){
-    var tname = document.createTextNode(input4);
-    tbldata.appendChild(tname);
-    }if(i==4 && j==0){
-        var tname = document.createTextNode(input5);
+    }if(i==3 && j==0){
+        var tname = document.createTextNode(input4);
         tbldata.appendChild(tname);
-        }if(i==5 && j==0){
-            var tname = document.createTextNode(input6);
+        }if(i==4 && j==0){
+            var tname = document.createTextNode(input5);
             tbldata.appendChild(tname);
-            }if(i==6 && j==0){
-                var tname = document.createTextNode(input7);
+            }if(i==5 && j==0){
+                var tname = document.createTextNode(input6);
                 tbldata.appendChild(tname);
-                }
-                if(i==7 && j==0){
-                    var tname = document.createTextNode(input8);
+                }if(i==6 && j==0){
+                    var tname = document.createTextNode(input7);
                     tbldata.appendChild(tname);
-                    }if(i==8 && j==0){
-                        var tname = document.createTextNode(input9);
+                    }
+                    if(i==7 && j==0){
+                        var tname = document.createTextNode(input8);
                         tbldata.appendChild(tname);
-                        }if(i==9 && j==0){
-                            var tname = document.createTextNode(input10);
+                        }if(i==8 && j==0){
+                            var tname = document.createTextNode(input9);
                             tbldata.appendChild(tname);
-                            }if(i==10 && j==0){
-                                var tname = document.createTextNode(input11);
+                            }if(i==9 && j==0){
+                                var tname = document.createTextNode(input10);
                                 tbldata.appendChild(tname);
-                                }
+                                }if(i==10 && j==0){
+                                    var tname = document.createTextNode(input11);
+                                    tbldata.appendChild(tname);
+                                    }
                 
 
-                                
-                                var input5 = document.querySelector("#email3").value;
-                                var input6 = document.querySelector("#email4").value;
-                                var input7 = document.querySelector("#email5").value;
+var input2 = document.querySelector("#email0").value;
+var input3 = document.querySelector("#email1").value;
+var input4 = document.querySelector("#email2").value;
+var input5 = document.querySelector("#email3").value;
+var input6 = document.querySelector("#email4").value;
+var input7 = document.querySelector("#email5").value;
 var input8 = document.querySelector("#email6").value;
-    var input9 = document.querySelector("#email7").value;
-    var input10 = document.querySelector("#email8").value;
-    var input11 = document.querySelector("#email9").value;
+var input9 = document.querySelector("#email7").value;
+var input10 = document.querySelector("#email8").value;
+var input11 = document.querySelector("#email9").value;
+
+
+
+
+
+
+    var einput = [];
+    var emailinput = document.querySelectorAll(".email");
+    var emailArray = [...emailinput];
+
+    for(var c=0; c<quan; c++){
+        einput[c] = emailArray.forEach(element);
+    }
+
+
+
+
+
+//likely to work
+    var x = document.querySelector("#email");
+    var w;
+    var einput = [];
+    for (w = 0; w < quan; w++) {
+      einput[w]=x[w].value;
+    }
+
+
+    if(i==1 && j==0){
+        var tname = document.createTextNode(emails[0].value);
+        tbldata.appendChild(tname);
+        }if(i==2 && j==0){
+            var tname = document.createTextNode(emails[1].value);
+            tbldata.appendChild(tname);
+             }if(i==3 && j==0){
+                var tname = document.createTextNode(emails[2].value);
+                tbldata.appendChild(tname);
+            }
